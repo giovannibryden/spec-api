4654f653c0b32b0af4d732423364ba7ec802fb2b
Giovanni <giovannibryden@gmail.com>
2015-10-23 15:48:20 -0700

    lol api

--- app/controllers/specs_controller.rb
+++ app/controllers/specs_controller.rb
@@ -1,10 +1,22 @@
 class SpecsController < ApplicationController
   
   def create
-  	
+  	@spec = Spec.new(spec_params)
+
+  	if @spec.save
+  		render :json => {:status => "1",:message => "Successfully created a spec.",:spec=> @spec}
+  	else
+  		render :json => {:status => "0",:message => "Spec was not successfully created."}
+  	end
   end
 
   def read
+  	@spec = Spec.find_by(key:params[:key])
+  	if @spec
+  		render :json => {:spec => @spec}
+  	else
+  		render :json => {:message => "Spec was not found."}
+  	end
   end
 
   def update
@@ -12,4 +24,10 @@ class SpecsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  	def spec_params
+  		params.permit(:spec,:key)
+  	end
 end
--- app/models/spec.rb
+++ app/models/spec.rb
@@ -1,2 +1,4 @@
 class Spec < ActiveRecord::Base
+	validates_presence_of :spec
+	validates_presence_of :key, uniqueness: {case_sensitive: false}
 end
